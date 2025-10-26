886cf0ec5dae0f55f70c9e9331bd93b4bc9191ae
Jeongmin An <ajhappy12@gmail.com>
2025-10-26 16:11:00 +0900

    Time: 0 ms (100%), Space: 8.5 MB (68.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        int n = str.length();
-
-        for (int i = 0; i < n / 2; i++) {
-            if (str[i] != str[n - i - 1]) {
-                return false;
-            }
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return true;
+        
+        int reversed = 0;
+        
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x == reversed || x == reversed / 10;
     }
 };

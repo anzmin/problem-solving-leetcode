f1db3910d3bc4de6e95bb55c1212440af572e50d
Jeongmin An <ajhappy12@gmail.com>
2025-10-26 16:06:06 +0900

    Time: 9 ms (8.38%), Space: 10.8 MB (15.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,6 +4,10 @@ public:
         string str = to_string(x);
         int n = str.length();
 
+        if (x < 0) {
+            return false;
+        }
+
         for (int i = 0; i < n / 2; i++) {
             if (str[i] != str[n - i - 1]) {
                 return false;

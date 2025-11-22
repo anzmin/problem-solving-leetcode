d93491a55d59e2afcffe5a2ef0b071802c0abb90
Jeongmin An <ajhappy12@gmail.com>
2025-11-23 00:15:33 +0900

    Time: 0 ms (100%), Space: 8.9 MB (12.62%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,58 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
+        int n = haystack.length();
+        int m = needle.length();
         
-        if (nLen > hLen) {
-            return -1;
-        }
+        if (m == 0) return 0;
         
-        for (int i = 0; i <= hLen - nLen; i++) {
-            int j = 0;
-            while (j < nLen && haystack[i + j] == needle[j]) {
+        vector<int> lps = computeLPS(needle);
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++;
                 j++;
-            }
-            
-            if (j == nLen) {
-                return i;
+                
+                if (j == m) {
+                    return i - j;
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
         
         return -1;
+    }
+    
+private:
+    vector<int> computeLPS(string needle) {
+        int m = needle.length();
+        vector<int> lps(m, 0);
+        int len = 0;
+        int i = 1;
+        
+        while (i < m) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps;
     }
 };

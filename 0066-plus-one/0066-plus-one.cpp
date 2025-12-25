482156ae832c7a30e1662b840e329fda7ce20fd5
Jeongmin An <ajhappy12@gmail.com>
2025-12-25 18:17:07 +0900

    Time: 0 ms (100%), Space: 11.6 MB (50.36%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int curr = digits[i] + carry;
+            carry = curr / 10;
+            digits[i] = curr - carry * 10;
+        }
+        if (carry == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
